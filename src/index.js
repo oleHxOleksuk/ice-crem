e764a3b58761735a96a4818db86ac9ff2ce00600
oleHxOleksuk <108345191+oleHxOleksuk@users.noreply.github.com>
2023-03-07 13:59:08 +0200

    34rtt

---
+++ src/index.js
@@ -0,0 +1,12 @@
+
+import('./js/mobile-menu');
+import('./js/modal-buy-now');
+import('./js/modal-buy-now-mobile');
+
+import('./js/contact-locations');
+import('./js/contact-franchise');
+import('./js/script-slider');
+import('./js/made-modal-js');
+import('./js/modal-product-one');
+import('./js/modal-product-two');
+import('./js/modal-product-three');
